0cad829f99792996e49508bea3a15144f0db2996
bman-prog <bmankovski2028@communityschoolnaples.org>
2024-11-21 12:29:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
 function calculateSuit (card: number) {
-    if (card / 13 == 0) {
-        return "Ace"
+    if (Math.idiv(card, 13) == 0) {
+        return "Spades"
+    } else if (Math.idiv(card, 13) == 1) {
+        return "Diamonds"
+    } else if (Math.idiv(card, 13) == 2) {
+        return "Clubs"
+    } else if (Math.idiv(card, 13) == 3) {
+        return "Hearts"
     } else {
-        return card / 13
+        return "0"
     }
 }
 function CalculatePip (card: number) {
-    if (card / 13 == 0) {
+    if (card % 13 == 10) {
+        return "Jack"
+    } else if (card % 13 == 11) {
+        return "Queen"
+    } else if (card % 13 == 12) {
+        return "King"
+    } else if (card % 13 == 0) {
         return "Ace"
     } else {
-        return card / 13
+        return convertToText(1 + card % 13)
     }
 }
 let myCard = game.askForNumber("Enter a number.")
